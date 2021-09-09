a294a8c4a0d05bc5db1c2db4820baca8f9d7f170
mebaysan <menesbaysan@gmail.com>
2021-09-09 07:11:48 +0300

    meta class section

--- fzthadmin/blog/models.py
+++ fzthadmin/blog/models.py
@@ -6,3 +6,10 @@ class Blog(models.Model):
     content = models.TextField()
     added_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'My Blog'
+        verbose_name_plural = 'My Blogs'
+
+    def __str__(self):
+        return self.title
