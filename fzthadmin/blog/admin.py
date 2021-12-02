1f0d1d178c4b9e53c083494ec686578bda588b9a
mebaysan <menesbaysan@gmail.com>
2021-12-02 11:31:46 +0300

    chapter3

--- fzthadmin/blog/admin.py
+++ fzthadmin/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from blog.models import Blog
 # Register your models here.
 
+
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'added_date', 'updated_date', 'is_published']
     list_per_page = 25
@@ -10,6 +11,27 @@ class BlogAdmin(admin.ModelAdmin):
     list_filter = ['is_published']
     prepopulated_fields = {'slug': ['title']}
     readonly_fields = ['added_date', 'updated_date']
+    date_hierarchy = 'updated_date'
+    actions = ('make_published',)
+    # fields = (
+    #     ('added_date','updated_date'),
+    #     ('title','slug'),
+    #     'content'
+    #     )
+    fieldsets = (
+        (None, {'fields': (('added_date', 'updated_date'),),
+         'description': 'Time Information'}),
+        ('Must Properties', {'fields': ('title', 'content'),
+         'description': 'The object has to have these attributes'}),
+        ('Nice to Have Properties', {'fields': (('slug', 'is_published'),),
+         'description': 'If the object hasn\'t these attributes, they will be automatically generated'})
+    )
+
+    def make_published(self, request, queryset):
+        count = queryset.update(is_published=True)
+        self.message_user(request, f"{count} objects changed")
+
+    make_published.short_description = 'Publish The Selected Blogs'
 
 
 admin.site.register(Blog, BlogAdmin)
